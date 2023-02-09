2fa15b3bb730f567a9c5e33efbd232f0999f01ef
robin.rytkonen <bobzoor@gmail.com>
2023-02-09 12:26:03 +0100

    "updated some unit tests"

--- src/test/java/com/work/javaconsumer/service/PlayerConsumerServiceTest.java
+++ src/test/java/com/work/javaconsumer/service/PlayerConsumerServiceTest.java
@@ -2,9 +2,10 @@ package com.work.javaconsumer.service;
 
 import com.work.javaconsumer.entity.Player;
 import com.work.javaconsumer.entity.PlayerRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -12,6 +13,7 @@ import org.work.kafka.api.RegisterPlayerDTO;
 import org.work.kafka.api.WinDepositDTO;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -24,20 +26,36 @@ class PlayerConsumerServiceTest {
     @InjectMocks
     PlayerConsumerService playerConsumerService;
 
+    @Captor
+    ArgumentCaptor<Player> playerArgumentCaptor;
+
     @Test
-    void should_find_one_player()  {
+    void should_find_one_player_by_email()  {
         //given
         RegisterPlayerDTO dto = new RegisterPlayerDTO("Bobba", "Bobzoor@gmail.com", 0);
         Player newPlayer = new Player(dto.getName(), dto.getEmail(), dto.getBalance());
 
-        when(playerRepository.findByEmail(newPlayer.getEmail())).thenReturn(Optional.of(newPlayer));
+        when(playerRepository.findByEmail(dto.getEmail())).thenReturn(Optional.of(newPlayer));
 
         //when
         playerConsumerService.registerPlayer(dto);
 
         //then
-        Assertions.assertEquals(dto.getName(), newPlayer.getName());
-        verify(playerRepository, times(1)).findByEmail(newPlayer.getEmail());
+        verify(playerRepository, times(1)).findByEmail(dto.getEmail());
+    }
+    @Test
+    void should_find_one_player_by_id()  {
+        //given
+        Player newPlayer = new Player("Bob", "Bobzoor@gmail.com", 0);
+        WinDepositDTO dto = new WinDepositDTO(newPlayer.getPlayerId(), 500);
+
+        when(playerRepository.findById(newPlayer.getPlayerId())).thenReturn(Optional.of(newPlayer));
+
+        //when
+        playerConsumerService.deposit(dto);
+
+        //then
+        verify(playerRepository, times(2)).findById(newPlayer.getPlayerId());
     }
 
     @Test
@@ -52,8 +70,10 @@ class PlayerConsumerServiceTest {
         playerConsumerService.registerPlayer(dto);
 
         //then
-        Assertions.assertEquals(newPlayer.getName(), dto.getName());
-        verify(playerRepository, times(1)).save(any(Player.class));
+        verify(playerRepository, times(1)).save(playerArgumentCaptor.capture()); // capture the object that is passed through the save method.
+        assertEquals("Bobba", playerArgumentCaptor.getValue().getName());
+        assertEquals("Bobzoor@gmail.com", playerArgumentCaptor.getValue().getEmail());
+        assertEquals(0, playerArgumentCaptor.getValue().getBalance());
     }
 
     @Test
@@ -68,9 +88,10 @@ class PlayerConsumerServiceTest {
         playerConsumerService.deposit(dto);
 
         //then
-        Assertions.assertEquals(500, newPlayer.getBalance());
         verify(playerRepository, times(2)).findById(dto.getPlayerId());
-        verify(playerRepository, times(1)).save(any(Player.class));
+        verify(playerRepository, times(1)).save(playerArgumentCaptor.capture());
+        assertEquals(500, playerArgumentCaptor.getValue().getBalance());
+        assertEquals("Bob", playerArgumentCaptor.getValue().getName());
+        assertEquals("Bobzoor@gmail.com", playerArgumentCaptor.getValue().getEmail());
     }
-
 }
